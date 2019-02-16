cde1b47180e7db8e7e0ad11fe56c3f1f2ac3bd6e
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-02-16 21:33:59 +0000

    r5900: decoder for 32-bit immediate shift

--- src/cc2/decode_mips.lua
+++ src/cc2/decode_mips.lua
@@ -36,7 +36,7 @@ local function function_field(instruction)
     return band(rshift(instruction, 26), 0x3F)
 end
 
-local function decode_instruction(decode_table, instruction)
+function instance:decode_instruction(instruction)
     local opcode = opcode(instruction)
     local first_source = first_source(instruction)
     local second_source = second_source(instruction)
@@ -44,15 +44,15 @@ local function decode_instruction(decode_table, instruction)
     local shift_amount = shift_amount(instruction)
     local function_field = function_field(instruction)
 
-    local opcode1 = decode_table[opcode + 1]
+    local opcode1 = self.decode_table[opcode + 1]
 
-    local opcode2_shift = opcode1[1]
-    local opcode2_mask = opcode1[2]
-    local opcode2_table = opcode1[3]
-    local opcode2_index = band(rshift(instruction, opcode2_shift), opcode2_mask)
-    local handler = opcode2_table[opcode2_index + 1]
+    local shift = opcode1[1]
+    local mask = opcode1[2]
+    local table = opcode1[3]
+    local index = band(rshift(instruction, shift), mask)
+    local handler = table[index + 1]
 
-    return handler(opcode, first_source, second_source, destination, shift_amount, function_field)
+    return handler(self, opcode, first_source, second_source, destination, shift_amount, function_field)
 end
 
 function instance:decode(read32, program_counter)
@@ -61,7 +61,7 @@ function instance:decode(read32, program_counter)
 
     while keep_decoding do
         local instruction = read32(program_counter)
-        local decode_ok, op = decode_instruction(self.decode_table, instruction)
+        local decode_ok, op = self:decode_instruction(instruction)
         keep_decoding = decode_ok
         ops[#ops+1] = op
     end
---
+++ src/cc2/r5900/decode.lua
@@ -0,0 +1,204 @@
+local decode_mips = require("cc2.decode_mips")
+
+local decode = {}
+
+local function illegal_instruction(_, _, _, _, _, _, _)
+    return false, ""
+end
+
+local function shift32_immediate(self, _, _, second_source, destination, shift_amount, function_field)
+    local op_table = {
+        [0x00] = "lshift", -- SLL
+        [0x02] = "rshift", -- SRL
+        [0x03] = "arshift" -- SRA
+    }
+
+    second_source = decode_mips.register_name(second_source)
+    destination = decode_mips.register_name(destination)
+
+    local op = {
+        self:declare_source(second_source),
+        self:declare_destination(destination),
+        destination,
+        " = ",
+        op_table[function_field],
+        "(",
+        second_source,
+        ", ",
+        tostring(shift_amount),
+        ")\n"
+    }
+
+    return table.concat(op)
+end
+
+local special_table = {
+    shift32_immediate,
+    illegal_instruction,
+    shift32_immediate,
+    shift32_immediate,
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+}
+
+local decode_table = {
+    { 0, 0x3F, special_table },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+}
+
+local function declare_source(self, register)
+    if self.declared[register] then
+        return ""
+    end
+    
+    self.declared[register] = true
+
+    if register ~= "zero" then
+        return "local " .. name .. " = s.gpr[" .. register .. "]\n"
+    end
+
+    return "local zero = 0\n"
+end
+
+local function declare_destination(self, register)
+    if self.declared[register] then
+        return ""
+    end
+
+    self.declared[register] = true
+
+    if register ~= "zero" then
+        return "local "
+    end
+
+    return ""
+end
+
+function decode:new()
+    local decoder = mips_decode:new(decode_table)
+    decoder.declare_source = declare_source
+    decoder.declare_destination = declare_destination
+    return decoder
+end
+
+return decode
