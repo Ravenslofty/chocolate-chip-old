b60fb62f4f7ebe8767d883caf964b7e2a23a4d8d
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-02-16 21:43:38 +0000

    r5900: add register names (woops)

--- src/cc2/decode_mips.lua
+++ src/cc2/decode_mips.lua
@@ -78,4 +78,43 @@ function mips_decode:new(decode_table)
     DecodeInstance)
 end
 
+local names = {
+    "zero",
+    "at",
+    "v0",
+    "v1",
+    "a0",
+    "a1",
+    "a2",
+    "a3",
+    "t0",
+    "t1",
+    "t2",
+    "t3",
+    "t4",
+    "t5",
+    "t6",
+    "t7",
+    "s0",
+    "s1",
+    "s2",
+    "s3",
+    "s4",
+    "s5",
+    "s6",
+    "s7",
+    "t8",
+    "t9",
+    "k0",
+    "k1",
+    "gp",
+    "sp",
+    "fp",
+    "ra"
+}
+
+function register_name(register)
+    return names[register]
+end
+
 return mips_decode
