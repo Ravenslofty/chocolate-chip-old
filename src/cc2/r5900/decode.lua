cbd88bd53c7e7f7190e39d68706ece42bbfd5a79
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-02-17 14:14:48 +0000

    r5900: bitwise operations, and shift cleanup

--- src/cc2/r5900/decode.lua
+++ src/cc2/r5900/decode.lua
@@ -15,12 +15,52 @@ local function illegal_instruction(_, _, _, _, _, _, _)
     return false, ""
 end
 
-local function shift32_immediate(self, _, _, second_source, destination, shift_amount, function_field)
+local function shift_immediate(self, _, _, second_source, destination, shift_amount, function_field)
     local op_table = {
-        [0x00] = "lshift", -- SLL
+        [0x00] = "lshift",  -- SLL
         -- 0x01 is illegal
-        [0x02] = "rshift", -- SRL
-        [0x03] = "arshift" -- SRA
+        [0x02] = "rshift",  -- SRL
+        [0x03] = "arshift", -- SRA
+        [0x38] = "lshift",  -- DSLL
+        -- 0x39 is illegal
+        [0x3A] = "rshift",  -- DSRL
+        [0x3B] = "arshift", -- DSRA
+        [0x3C] = "lshift",  -- DSLL32
+        -- 0x3D is illegal
+        [0x3E] = "rshift",  -- DSRL32
+        [0x3F] = "arshift"  -- DSRA32
+    }
+
+    -- -32 instructions use shift_amount + 32
+    local shift_base = {
+        [0x00] = 0,  -- SLL
+        -- 0x01 is illegal
+        [0x02] = 0,  -- SRL
+        [0x03] = 0,  -- SRA
+        [0x38] = 0,  -- DSLL
+        -- 0x39 is illegal
+        [0x3A] = 0,  -- DSRL
+        [0x3B] = 0,  -- DSRA
+        [0x3C] = 32, -- DSLL32
+        -- 0x3D is illegal
+        [0x3E] = 32, -- DSRL32
+        [0x3F] = 32  -- DSRA32
+    }
+
+    -- 32-bit instructions need sign-extension
+    local needs_sign_extend = {
+        [0x00] = true,  -- SLL
+        -- 0x01 is illegal
+        [0x02] = true,  -- SRL
+        [0x03] = true,  -- SRA
+        [0x38] = false, -- DSLL
+        -- 0x39 is illegal
+        [0x3A] = false, -- DSRL
+        [0x3B] = false, -- DSRA
+        [0x3C] = false, -- DSLL32
+        -- 0x3D is illegal
+        [0x3E] = false, -- DSRL32
+        [0x3F] = false  -- DSRA32
     }
 
     second_source = decode_mips.register_name(second_source)
@@ -39,96 +79,49 @@ local function shift32_immediate(self, _, _, second_source, destination, shift_a
         ", ",
         tostring(shift_amount),
         ")\n",
-        -- Sign-extend
-        sign_extend_32_64(destination)
     }
+
+    if needs_sign_extend[function_field] then
+        op[#op + 1] = sign_extend_32_64(destination)
+    end
 
     return table.concat(op)
 end
 
-local function shift32_variable(self, _, first_source, second_source, destination, _, function_field)
+local function shift_variable(self, _, first_source, second_source, destination, _, function_field)
     local op_table = {
-        [0x04] = "lshift", -- SLLV
+        [0x04] = "lshift",  -- SLLV
         -- 0x05 is illegal
-        [0x06] = "rshift", -- SRLV
-        [0x07] = "arshift" -- SRAV
-    }
-
-    first_source = decode_mips.register_name(first_source)
-    second_source = decode_mips.register_name(second_source)
-    destination = decode_mips.register_name(destination)
-
-    local op = {
-        -- Operands
-        self:declare_source(first_source),
-        self:declare_source(second_source),
-        self:declare_destination(destination),
-        -- Shift
-        destination,
-        " = ",
-        op_table[function_field],
-        "(",
-        second_source,
-        ", band(",
-        first_source,
-        "0x1F))\n",
-        -- Sign-extend
-        sign_extend_32_64(destination)
-    }
-
-    return table.concat(op)
-end
-
-local function shift64_immediate(self, _, _, second_source, destination, shift_amount, function_field)
-    local op_table = {
-        [0x38] = "lshift",  -- DSLL
-        -- 0x39 is illegal
-        [0x3A] = "rshift",  -- DSRL
-        [0x3B] = "arshift", -- DSRA
-        [0x3C] = "lshift",  -- DSLL32
-        -- 0x3D is illegal
-        [0x3E] = "rshift",  -- DSRL32
-        [0x3F] = "arshift"  -- DSRA32
-    }
-
-    local shift_base = {
-        [0x38] = 0,  -- DSLL
-        -- 0x39 is illegal
-        [0x3A] = 0,  -- DSRL
-        [0x3B] = 0,  -- DSRA
-        [0x3C] = 32, -- DSLL32
-        -- 0x3D is illegal
-        [0x3E] = 32, -- DSRL32
-        [0x3F] = 32  -- DSRA32
-    }
-
-    second_source = decode_mips.register_name(second_source)
-    destination = decode_mips.register_name(destination)
-
-    local op = {
-        -- Operands
-        self:declare_source(second_source),
-        self:declare_destination(destination),
-        -- Shift
-        destination,
-        " = ",
-        op_table[function_field],
-        "(",
-        second_source,
-        ", ",
-        tostring(shift_base[function_field] + shift_amount),
-        ")\n"
-    }
-
-    return table.concat(op)
-end
-
-local function shift64_variable(self, _, first_source, second_source, destination, _, function_field)
-    local op_table = {
-        [0x14] = "lshift", -- DSLLV
+        [0x06] = "rshift",  -- SRLV
+        [0x07] = "arshift", -- SRAV
+        [0x14] = "lshift",  -- DSLLV
         -- 0x15 is illegal
-        [0x16] = "rshift", -- DSRLV
-        [0x17] = "arshift" -- DSRAV
+        [0x16] = "rshift",  -- DSRLV
+        [0x17] = "arshift"  -- DSRAV
+    }
+
+    -- 32-bit instructions need sign-extension
+    local needs_sign_extend = {
+        [0x04] = true,  -- SLLV
+        -- 0x05 is illegal
+        [0x06] = true,  -- SRLV
+        [0x07] = true,  -- SRAV
+        [0x14] = false, -- DSLLV
+        -- 0x15 is illegal
+        [0x16] = false, -- DSRLV
+        [0x17] = false  -- DSRAV
+    }
+
+    -- 32-bit instructions use the low 5 bits, 64-bit instructions use the low 6 bits.
+    local mask = {
+        [0x04] = 0x1F, -- SLLV
+        -- 0x05 is illegal
+        [0x06] = 0x1F, -- SRLV
+        [0x07] = 0x1F, -- SRAV
+        [0x14] = 0x3F, -- DSLLV
+        -- 0x15 is illegal
+        [0x16] = 0x3F, -- DSRLV
+        [0x17] = 0x3F  -- DSRAV
     }
 
     first_source = decode_mips.register_name(first_source)
@@ -148,86 +141,119 @@ local function shift64_variable(self, _, first_source, second_source, destinatio
         second_source,
         ", band(",
         first_source,
-        "0x3F))\n",
+        ",",
+        tostring(mask[function_field]),
+        "))\n",
     }
 
     return table.concat(op)
 end
 
+local function bitop_register(self, _, first_source, second_source, destination, _, function_field)
+    local op_table = {
+        [0x24] = "band",     -- AND
+        [0x25] = "bor",      -- OR
+        [0x16] = "bxor",     -- XOR
+        [0x17] = "bnot(bor(" -- NOR
+    }
+
+    local end_bracket = {
+        [0x24] = "", -- AND
+        [0x25] = "", -- OR
+        [0x26] = "", -- XOR
+        [0x27] = ")" -- NOR
+    }
+
+    first_source = decode_mips.register_name(first_source)
+    second_source = decode_mips.register_name(second_source)
+    destination = decode_mips.register_name(destination)
+
+    local op = {
+        -- Operands
+        self:declare_source(first_source),
+        self:declare_source(second_source),
+        self:declare_destination(destination),
+        -- Shift
+        destination,
+        " = ",
+        op_table[function_field],
+        "(",
+        first_source,
+        ", ",
+        second_source,
+        ")",
+        end_bracket[function_field],
+        "\n"
+    }
+
+    return table.concat(op)
+end
 
 local special_table = {
-    shift32_immediate,      -- SLL
+    shift_immediate,        -- SLL
     illegal_instruction,
-    shift32_immediate,      -- SRL
-    shift32_immediate,      -- SRA
-    shift32_variable,       -- SLLV
+    shift_immediate,        -- SRL
+    shift_immediate,        -- SRA
+    shift_variable,         -- SLLV
     illegal_instruction,
-    shift32_variable,       -- SRLV
-    shift32_variable,       -- SRAV
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    shift64_variable,       -- DSLLV
+    shift_variable,         -- SRLV
+    shift_variable,         -- SRAV
+    {},                     -- JR
+    {},                     -- JALR
+    {},                     -- MOVZ
+    {},                     -- MOVN
+    {},                     -- SYSCALL
+    {},                     -- BREAK
     illegal_instruction,
-    shift64_variable,       -- DSRLV
-    shift64_variable,       -- DSRAV
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    shift32_immediate,      -- DSLL
+    {},                     -- SYNC
+    {},                     -- MFHI
+    {},                     -- MTHI
+    {},                     -- MFLO
+    {},                     -- MTLO
+    shift_variable,         -- DSLLV
     illegal_instruction,
-    shift32_immediate,      -- DSRL
-    shift32_immediate,      -- DSRA
-    shift32_immediate,      -- DSLL32
+    shift_variable,         -- DSRLV
+    shift_variable,         -- DSRAV
+    {},                     -- MULT
+    {},                     -- MULTU
+    {},                     -- DIV
+    {},                     -- DIVU
+    illegal_instruction,    -- (DMULT)
+    illegal_instruction,    -- (DMULTU)
+    illegal_instruction,    -- (DDIV)
+    illegal_instruction,    -- (DDIVU)
+    {},                     -- ADD
+    {},                     -- ADDU
+    {},                     -- SUB
+    {},                     -- SUBU
+    bitop_register,         -- AND
+    bitop_register,         -- OR
+    bitop_register,         -- XOR
+    bitop_register,         -- NOR
+    {},                     -- MFSA
+    {},                     -- MTSA
+    {},                     -- SLT
+    {},                     -- SLTU
+    {},                     -- DADD
+    {},                     -- DADDU
+    {},                     -- DSUB
+    {},                     -- DSUBU
+    {},                     -- TGE
+    {},                     -- TGEU
+    {},                     -- TLT
+    {},                     -- TLTU
+    {},                     -- TEQ
     illegal_instruction,
-    shift32_immediate,      -- DSRL32
-    shift32_immediate,      -- DSRA32
+    {},                     -- TNE
+    illegal_instruction,
+    shift_immediate,        -- DSLL
+    illegal_instruction,
+    shift_immediate,        -- DSRL
+    shift_immediate,        -- DSRA
+    shift_immediate,        -- DSLL32
+    illegal_instruction,
+    shift_immediate,        -- DSRL32
+    shift_immediate,        -- DSRA32
 }
 
 local decode_table = {
